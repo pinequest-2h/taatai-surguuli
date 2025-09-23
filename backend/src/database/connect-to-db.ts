0454a612c68350e38f3176b436f926c59a3389ea
Otgontsetseg <dorjotgoo@Otgontsetsegs-MacBook-Air.local>
2025-09-23 09:04:04 +0800

    again

--- backend/src/database/connect-to-db.ts
+++ backend/src/database/connect-to-db.ts
@@ -17,8 +17,6 @@ export const connectToDb = async () => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      bufferCommands: false, // Disable mongoose buffering
       maxIdleTimeMS: 30000, // Close connections after 30 seconds of inactivity
       retryWrites: true,
     };
